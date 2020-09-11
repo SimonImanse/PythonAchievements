afbc6e3d1eac104ab902f78d69f2c6da572e1716
SimonImanse <30230@ma-web.nl>
2020-09-11 15:27:08 +0200

    opdracht poem gemaakt

---
+++ PythonAchievements/poem printing.py
@@ -0,0 +1,10 @@
+vers1 = "Simon, yes, that's my name," 
+vers2 = "Ik leer Software-dev, dus je weet dat ik game."
+vers3 = "Ik woon in Voorhout. Waar? Oh, dat ligt naast Leiden."
+vers4 = "Ik ben sociaal, maar wifi en ik zal je niet scheiden."
+vers5 = "Met vrienden op straat, nee ik toon geen haat."
+vers6 = "Want helemaal eerlijk, ik vind niet dat het mij staat."
+vers7 = "Sorry voor dit verhaal. ik weet het klinkt super raar,"
+vers9 = "maar ben wel opgelucht, want ik ben eindelijk klaar."
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers9)
