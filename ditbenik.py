5450a39a1b66477ed5888907abcf100dfcb30b65
SimonImanse <30230@ma-web.nl>
2020-10-20 14:47:57 +0200

    opdracht ditbenik klaar

--- ditbenik.py
+++ ditbenik.py
@@ -6,7 +6,7 @@ choice = input("Welke kleur haar heb ik? A: Blond, B: Rossig of C: Bruin?")
 if choice == 'A' or choice == 'C':
 	print("FOUT, probeer opnieuw!")
 elif choice == 'B': 
-	print("GOED!")
+    print("GOED!")
     choice2 = input("Vraag 2: Wat is mij favoriete kleur? A: Rood, B: Bruin of C: Groen?")
     if choice2 == 'A' or choice2 == 'B':
         print("FOUT, probeer opnieuw!")
@@ -14,9 +14,9 @@ elif choice == 'B':
         print("GOED!")
         choice3 = input("Laatste vraag: Wat is mijn favoriete game? A: Fortnite, B: League of Legends of C: Call of Duty?")
         if choice3 == 'A' or choice3 == 'C':
-	    print("FOUT, probeer opnieuw!")
+	        print("FOUT, probeer opnieuw!")
         elif choice3 == 'B': 
-	    print("GOED! Je hebt de quiz perfect gemaakt!")
+	        print("GOED! Je hebt de quiz perfect gemaakt!")
         else:
             print("Verkeerd ingevoerd, probeer opnieuw!")
     else:
